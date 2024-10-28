6385fa4fc12f0491f94ed4a61667da1bdae56bef
MNegi17 <manannegi17@gmai.com>
2024-10-28 23:51:03 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+
+import { blogRouter } from './routes/blog'
+import { userRouter } from './routes/user'
+
+
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string
+  }
+}>()
+
+app.route("/api/v1/user", userRouter)
+app.route("/api/v1/blog", blogRouter)
+
+
+export default app
